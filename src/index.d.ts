a975b6f402d20e4a9fb5df0b9a23922b0a6bf890
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-04-18 20:53:00 +0200

    Fix styling

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,20 +1,19 @@
-import {EventEmitter2 } from 'eventemitter2'
+import { EventEmitter2 } from 'eventemitter2'
 import ROSLIB from 'roslib'
 
 export class AutoRos extends EventEmitter2 {
 
-	public ros: ROSLIB.Ros;
+    public ros: ROSLIB.Ros;
 
-	constructor(options?:{
-		reconnectTimeOut?: number,
-		rosOptions?: {
-			encoding?: string,
-		}
-        });
+    constructor(options?: {
+        reconnectTimeOut?: number,
+        rosOptions?: {
+            encoding?: string,
+        }
+    });
 
-	connect(url:string):void;
+    connect(url: string): void;
 
 }
 
 export default AutoRos;
-
