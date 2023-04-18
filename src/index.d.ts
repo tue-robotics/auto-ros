af51844608e0001d33778c020e1240433398e764
Janno Lunenburg <jlunenburg@users.noreply.github.com>
2023-04-18 20:57:12 +0200

    Feature/typing (#277)
    * wip()Adding typing
    
    * fix()Added missing file
    
    * feat()Added typing
    
    * fix()Removed url from typing
    
    * Fix styling
    
    ---------
    
    Co-authored-by: Janno Lunenburg <jlunenburg@smart-robotics.nl>
    Co-authored-by: Matthijs van der Burgh <MatthijsBurgh@outlook.com>

---
+++ src/index.d.ts
@@ -0,0 +1,19 @@
+import { EventEmitter2 } from 'eventemitter2'
+import ROSLIB from 'roslib'
+
+export class AutoRos extends EventEmitter2 {
+
+    public ros: ROSLIB.Ros;
+
+    constructor(options?: {
+        reconnectTimeOut?: number,
+        rosOptions?: {
+            encoding?: string,
+        }
+    });
+
+    connect(url: string): void;
+
+}
+
+export default AutoRos;
