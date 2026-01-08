98e790b7464d0704d7934ee1fc0f68a1f23bbb2a
Matthijs van der Burgh <matthijs.van.der.burgh@vbti.nl>
2026-01-08 10:17:17 +0100

    Update chai and sinon-chai

--- test/test.ts
+++ test/test.ts
@@ -1,25 +1,23 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-nocheck
 import * as chai from 'chai'
 import sinonChai from 'sinon-chai'
 import { stub, useFakeTimers, type SinonStub, type SinonFakeTimers } from 'sinon'
 import { AutoRos } from '../src/index.js'
+import { expect } from 'chai'
 
 chai.use(sinonChai)
-chai.should()
 
 const exampleUrl = 'ws://example.com:9090'
 
 describe('AutoRos', () => {
   const options = {}
   let autoRos: AutoRos
-  
+
   beforeEach('create a AutoRos', () => {
     autoRos = new AutoRos(options)
   })
 
   let connect: SinonStub
-  
+
   beforeEach('Stub connect', () => {
     connect = stub(autoRos.ros, 'connect')
     connect.resolves()
@@ -34,28 +32,28 @@ describe('AutoRos', () => {
      * Tests
      */
     it('Should connect to a custom url', () => {
-      connect.should.have.not.been.called
+      expect(connect).to.not.have.been.called
       autoRos.connect(exampleUrl)
-      connect.should.have.been.calledOnce
-      connect.should.have.been.calledWithExactly(exampleUrl)
+      expect(connect).to.have.been.calledOnce
+      expect(connect).to.have.been.calledWithExactly(exampleUrl)
     })
 
     it('Should connect to a default url', () => {
-      connect.should.have.not.been.called
+      expect(connect).to.not.have.been.called
       autoRos.connect()
-      connect.should.have.been.calledOnce
-      connect.should.have.been.calledWithMatch(/ws:\/\/[a-zA-Z\d-.]+:9090/)
+      expect(connect).to.have.been.calledOnce
+      expect(connect).to.have.been.calledWithMatch(/ws:\/\/[a-zA-Z\d-.]+:9090/)
     })
 
     it('Should remember the previous url', () => {
-      connect.should.have.not.been.called
+      expect(connect).to.not.have.been.called
 
       autoRos.connect(exampleUrl)
-      connect.should.have.been.calledOnce
+      expect(connect).to.have.been.calledOnce
 
       autoRos.connect()
-      connect.should.have.been.calledTwice
-      connect.should.always.have.been.calledWithExactly(exampleUrl)
+      expect(connect).to.have.been.calledTwice
+      expect(connect).to.always.have.been.calledWithExactly(exampleUrl)
     })
   })
 
@@ -64,7 +62,7 @@ describe('AutoRos', () => {
      * Stubs
      */
     let clock: SinonFakeTimers
-    
+
     beforeEach('Stub setTimeout', () => {
       clock = useFakeTimers()
     })
@@ -77,43 +75,40 @@ describe('AutoRos', () => {
      * Tests
      */
     it('Should have a default status', () => {
-      autoRos.status.should.equal('closed')
+      expect(autoRos.status).to.equal('closed')
     })
 
     it('Should respond to the ros::connection event', () => {
       autoRos.connect(exampleUrl)
-      autoRos.status.should.equal('connecting')
-      connect.should.have.been.calledOnce
-      connect.should.have.been.calledWithExactly(exampleUrl)
+      expect(autoRos.status).to.equal('connecting')
+      expect(connect).to.have.been.calledOnce
+      expect(connect).to.have.been.calledWithExactly(exampleUrl)
 
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      autoRos.ros.emit('connection', {} as any)
-      autoRos.status.should.equal('connected')
+      autoRos.ros.emit('connection', {})
+      expect(autoRos.status).to.equal('connected')
     })
 
     it('Should reconnect after the ros::closed event', () => {
       autoRos.connect(exampleUrl)
-      connect.should.have.been.calledOnce
-      connect.should.have.been.calledWithExactly(exampleUrl)
+      expect(connect).to.have.been.calledOnce
+      expect(connect).to.have.been.calledWithExactly(exampleUrl)
 
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      autoRos.ros.emit('close', {} as any)
-      autoRos.status.should.equal('closed')
+      autoRos.ros.emit('close', {})
+      expect(autoRos.status).to.equal('closed')
       clock.tick(100)
 
-      connect.should.have.been.calledOnce
-      connect.should.have.been.calledWithExactly(exampleUrl)
+      expect(connect).to.have.been.calledOnce
+      expect(connect).to.have.been.calledWithExactly(exampleUrl)
 
       clock.tick(5000)
 
-      connect.should.have.been.calledTwice
-      connect.should.always.have.been.calledWithExactly(exampleUrl)
+      expect(connect).to.have.been.calledTwice
+      expect(connect).to.always.have.been.calledWithExactly(exampleUrl)
     })
 
     it('Should respond to the ros::error event', () => {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      autoRos.ros.emit('error', {} as any)
-      autoRos.status.should.equal('error')
+      autoRos.ros.emit('error', {})
+      expect(autoRos.status).to.equal('error')
     })
   })
 
@@ -122,13 +117,13 @@ describe('AutoRos', () => {
      * Stubs
      */
     let customAutoRos: AutoRos
-    
+
     beforeEach('Create AutoRos with custom timeout', () => {
       customAutoRos = new AutoRos({ reconnectTimeOut: 10000 })
     })
 
     let customConnect: SinonStub
-    
+
     beforeEach('Stub connect', () => {
       customConnect = stub(customAutoRos.ros, 'connect')
       customConnect.resolves()
@@ -139,7 +134,7 @@ describe('AutoRos', () => {
     })
 
     let clock: SinonFakeTimers
-    
+
     beforeEach('Stub setTimeout', () => {
       clock = useFakeTimers()
     })
@@ -153,21 +148,20 @@ describe('AutoRos', () => {
      */
     it('Should reconnect after the ros::closed event', () => {
       customAutoRos.connect(exampleUrl)
-      customConnect.should.have.been.calledOnce
-      customConnect.should.have.been.calledWithExactly(exampleUrl)
+      expect(customConnect).to.have.been.calledOnce
+      expect(customConnect).to.have.been.calledWithExactly(exampleUrl)
 
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      customAutoRos.ros.emit('close', {} as any)
-      customAutoRos.status.should.equal('closed')
+      customAutoRos.ros.emit('close', {})
+      expect(customAutoRos.status).to.equal('closed')
       clock.tick(5100)
 
-      customConnect.should.have.been.calledOnce
-      customConnect.should.have.been.calledWithExactly(exampleUrl)
+      expect(customConnect).to.have.been.calledOnce
+      expect(customConnect).to.have.been.calledWithExactly(exampleUrl)
 
       clock.tick(5000)
 
-      customConnect.should.have.been.calledTwice
-      customConnect.should.always.have.been.calledWithExactly(exampleUrl)
+      expect(customConnect).to.have.been.calledTwice
+      expect(customConnect).to.always.have.been.calledWithExactly(exampleUrl)
     })
   })
 })
@@ -178,6 +172,6 @@ describe('rosOptions.url is not allowed', () => {
     const badConstructor = function() {
       return new AutoRos(options)
     }
-    badConstructor.should.throw(/url/)
+    expect(badConstructor).to.throw(/url/)
   })
 })
